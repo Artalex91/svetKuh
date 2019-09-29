ab8053c77fa4432a2d550bce48450e045c21669d
Artalex <mobart.91@mail.ru>
2019-09-29 15:58:00 +0300

    start

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+//ver 2.0.0
+#include <Arduino.h>
+
+#define led1Pin // 40см
+#define led2Pin // 100см
+#define led3Pin // 60см
+#define pirPin //
+#define but1Pin //
+#define but2Pin //
+
+void setup() {
+
+}
+
+void loop() {
+
+}
